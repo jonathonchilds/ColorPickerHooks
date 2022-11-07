e592caf17e687743210e47487fd9ff1c3800c316
jonathonchilds <33766594+jonathonchilds@users.noreply.github.com>
2022-11-07 16:07:27 -0700

    adds state to slider - unable to make connection for value to update state

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
 export function App() {
+  const [value, setValue] = useState(22)
+
+  //function handleChange(e) {}
   return (
     <div>
       <div className="picker-container">
         <h1>Color Picker</h1>
-        <div>
-          <input type="range" id="hue" name="hue" min="0" max="360" />
-          <label htmlFor="hue">Hue</label>
+        <div className="slider-container">
+          <input
+            onInput={() => setValue}
+            type="range"
+            id="hue"
+            name="hue"
+            min="0"
+            max="360"
+            step="1"
+            //value={setValue}
+          />
+          <label className="slider-labels" htmlFor="hue">
+            Hue
+            <div>{value}</div>
+          </label>
         </div>
-        <div>
+        <div className="slider-container">
           <input
             type="range"
             id="saturation"
             name="saturation"
             min="0"
             max="100"
+            step="1"
           />
-          <label htmlFor="saturation">Saturation</label>
+          <label className="slider-labels" htmlFor="saturation">
+            Saturation
+          </label>
         </div>
-        <div>
+        <div className="slider-container">
           <input
             type="range"
             id="lightness"
             name="lightness"
             min="0"
             max="100"
+            step="1"
           />
-          <label htmlFor="lightness">Lightness</label>
+          <label className="slider-labels" htmlFor="lightness">
+            Lightness
+          </label>
         </div>
         <button>Random Color</button>
       </div>
