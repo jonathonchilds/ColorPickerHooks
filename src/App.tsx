d4b0f4e5bdb68eddc5d7831275c8cbf766a111e5
jonathonchilds <33766594+jonathonchilds@users.noreply.github.com>
2022-11-07 14:05:15 -0700

    changes background color and adds three range inputs

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 import React from 'react'
 
 export function App() {
-  return <div>Hello, World </div>
+  return (
+    <main>
+      <p className="#styling"> Color Picker</p>
+      <input type="range" />
+      <input type="range" />
+      <input type="range" />
+    </main>
+  )
 }
