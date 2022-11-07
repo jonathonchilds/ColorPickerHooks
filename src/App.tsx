c119434a72c42aff1208c85b6c39aa2c8672d412
jonathonchilds <33766594+jonathonchilds@users.noreply.github.com>
2022-11-07 14:41:43 -0700

    adds basic CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
 import React from 'react'
+import './App.css'
 
 export function App() {
   return (
-    <main>
-      <p className="#styling"> Color Picker</p>
-      <input type="range" />
-      <input type="range" />
-      <input type="range" />
-    </main>
+    <div>
+      <div className="picker-styling">
+        Color Picker
+        <div>
+          <input type="range" id="hue" name="hue" min="0" max="360" />
+        </div>
+        <div>
+          <input
+            type="range"
+            id="saturation"
+            name="saturation"
+            min="0"
+            max="100"
+          />
+        </div>
+        <div>
+          <input
+            type="range"
+            id="lightness"
+            name="lightness"
+            min="0"
+            max="100"
+          />
+        </div>
+      </div>
+    </div>
   )
 }
